e0af67c2e59d6eb4e6d939e172d785d5e79d0a48
NepalJohn89 <124467776+NepalJohn89@users.noreply.github.com>
2023-03-02 21:29:51 -0300

    set up

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
 var inputs = []
 var BadApple = {
-    frame:0
+    frame:1,
+    items:{
+        _1:[
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+        ]
+    }
 }
 
 for (let i = 0; i < 100; i++) {
     inputs.push(document.querySelector("#input" + (i+1)))
 }
+
 document.querySelector(".data-loading").style.display = "none"
+
+setInterval(() => {
+    for (let i = 0; i < inputs.length; i++) {
+        inputs[i].style.opacity = BadApple.items["_" + BadApple.frame][i]
+    }
+}, 50);
