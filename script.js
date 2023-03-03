16090a9d50a30710fc057dc23e1e013811b31ae5
NepalJohn89 <124467776+NepalJohn89@users.noreply.github.com>
2023-03-02 21:16:40 -0300

    get a now

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 var inputs = []
+var BadApple = {
+    frame:0
+}
 
 for (let i = 0; i < 100; i++) {
     inputs.push(document.querySelector("#input" + (i+1)))
 }
+document.getElementById(".data-loading").style.display = "none"
